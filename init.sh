c7311c9e79fb383d782c36fdc55db11ce87bf26d
Nicholas Goodwin <theunforgiven@gmail.com>
2014-06-27 12:50:38 +0000

    mark init script executable, prevent vim directory symbolic link creation from creating a link in the .vim directory if it already exists -> now fails with an error

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-ln -s .fanseashell/tmux.conf .tmux.conf
-ln -s .fanseashell/vimrc .vimrc
-ln -s .fanseashell/vim .vim
+#!/bin/sh
+ln -s ~/.fanseashell/tmux.conf ~/.tmux.conf
+ln -s ~/.fanseashell/vimrc ~/.vimrc
+ln -s -T ~/.fanseashell/vim ~/.vim
